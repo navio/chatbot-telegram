6a13f6163dacc7d42309ef9c4e72e06763177d77
Alberto Navarro <alnavarro@gmail.com>
2023-11-01 21:32:48 -0400

    Updates and Clean Up

--- src/botMaster.ts
+++ src/botMaster.ts
@@ -21,6 +21,9 @@ export class BotMaster {
     this.bot = new TelegramBot(this.botToken, { polling: true });
     this.db = new Memory();
     this.gpt = new GptAssistant(this.openAiApiKey);
+
+    // binding to use everywhere.
+    this.onMessage = this.onMessage.bind(this);
   }
 
   static async handleStockEvent(msg: any) {
@@ -63,143 +66,176 @@ export class BotMaster {
     return interval;
   }
 
-  start() {
-    // Default message event listener
-    this.bot.on("message", async (msg: any) => {
-      console.log("userID", msg.from.username);
-      console.log("chatID", msg.chat.id);
+  async triggers(messageText: string, chatId: number) {
 
-      const messageText = msg?.text?.trim() || "";
-      if (!messageText) return;
+    let response
 
-      const chatId = msg.chat.id;
-      const username = msg.from.username;
-      const fromId = msg.from.id;
-
-      let response = undefined;
-      // Stock event listener
-      if (messageText.startsWith("/stock")) {
-        response = await BotMaster.handleStockEvent(messageText);
-      }
-      // GPT4 event listener
-      else if (messageText.startsWith("*gpt4")) {
-        const interval = this.startTypingInterval(chatId);
-        response = await this.handleGPTEventInternal(
-          messageText.slice(5).trim(),
-          chatId,
-          {
-            model: "gpt-4",
-          }
-        );
-        clearInterval(interval);
-      }
-      // GPT event listener
-      else if (messageText.startsWith("*gpt")) {
-        response = await this.handleGPTEventInternal(
-          messageText.slice(4).trim(),
-          chatId
-        );
-      }
-      // Process other messages
-      else {
-        // Process stock symbols starting with $
-        const stockRegex = /\$[a-zA-Z]+\b/g;
-        const stocks = messageText.match(stockRegex);
-
-        if (stocks && stocks.length) {
-          for (const stock of stocks) {
-            const stockSymbol = stock.slice(1); // Remove the '$' symbol
-            response = await stockRetriever(stockSymbol);
-            response && this.bot.sendMessage(chatId, response);
-          }
-          return;
+    if (messageText.startsWith("/stock")) { // Stock event listener
+      response = await BotMaster.handleStockEvent(messageText);
+    }
+    else if (messageText.startsWith("*gpt4")) { // GPT4 event listener
+      const interval = this.startTypingInterval(chatId);
+      response = await this.handleGPTEventInternal(
+        messageText.slice(5).trim(),
+        chatId,
+        {
+          model: "gpt-4",
         }
+      );
+      clearInterval(interval);
+    }
+    else if (messageText.startsWith("*gpt")) { // GPT event listener
+      response = await this.handleGPTEventInternal(
+        messageText.slice(4).trim(),
+        chatId
+      );
+    }
 
-        // Personal Code
+    return response;
+  }
 
-        // this.bot.sendChatAction(chatId, "typing");
-        const checkForKeys = (messageText: string) => {
-          if (messageText.startsWith("/start")) {
-            return "start";
-          }
-          if (messageText.startsWith("/simple")) {
-            return "simple";
-          }
-          if (messageText.startsWith("/turbo")) {
-            return "turbo";
-          }
-          if (messageText.startsWith("/clear")) {
-            return "clear";
-          }
-          if (
-            messageText.startsWith("$stock") ||
-            messageText.startsWith("$gpt4") ||
-            messageText.startsWith("$gpt")
-          ) {
-            return "skip";
-          }
-          return;
-        };
-        const keys = checkForKeys(messageText);
-        if ( this.members.includes(username) && chatId === fromId) {
-          switch (keys) {
-            case "start":
-              this.bot.sendMessage(chatId, "Welcome to the bot!");
-              break;
-            case "simple":
-              console.log("simple");
-              const fixed = msg.text.slice(7).trim();
-              const responseTextTurbo = await this.gpt.fetchGptResponseTurbo(
-                fixed,
-                []
-              );
-              this.bot.sendMessage(chatId, responseTextTurbo);
-              break;
-            case "turbo":
-              console.log("turbo");
-              const fixedTurbo = msg.text.slice(6).trim();
-              const interval = setInterval(() => {
-                this.bot.sendChatAction(chatId, "typing");
-              }, 2000);
-              const response = this.gpt.fetchGptResponseTurbo(fixedTurbo, []);
-              clearInterval(interval);
-              this.bot.sendMessage(chatId, response, {
-                parse_mode: "HTML",
-              });
-              break;
-            case "clear":
-              this.db.clearMessages(chatId);
-              this.bot.sendMessage(chatId, "Messages cleared");
-              break;
-            case "skip":
-              break;
-            default: {
-              // start typing action
-              const interval = this.startTypingInterval(chatId);
-              // get response
-              const response = await this.handleGPTEventInternal(
-                messageText,
-                chatId,
-                {
-                  // model: "gpt-4",
-                }
-              );
-              clearInterval(interval);
-              // send response
-              this.bot.sendMessage(chatId, response, {
-                parse_mode: "HTML",
-              });
+  async onMessage(msg: any) {
+    console.log("message from userID: ", msg.from.username);
+    console.log("on chatID", msg.chat.id);
+    console.log("from id", msg.from.id)
+
+    const chatId = msg.chat.id;
+    const username = msg.from.username;
+    const fromId = msg.from.id;
+
+    const chats = [-569513240]
+
+    const isActiveGroupChat = chats.includes(Number(chatId));
+    const isActivePrivateChat = this.members.includes(username) && chatId === fromId;
+
+    const messageText = msg?.text?.trim() || "";
+    if (!messageText) return;
+
+    if (!isActiveGroupChat && !isActivePrivateChat) return;
+
+    let response;
+
+    // Process stock symbols starting with $
+    const stockRegex = /\$[a-zA-Z]+\b/g;
+    const stocks = messageText.match(stockRegex);
+
+    if (stocks && stocks.length) {
+      for (const stock of stocks) {
+        const stockSymbol = stock.slice(1); // Remove the '$' symbol
+        response = await stockRetriever(stockSymbol);
+        response && this.bot.sendMessage(chatId, response);
+      }
+      return;
+    }
+
+    const createRegexMatchingWords = (words: string[]) => {
+      // Escape special regex characters
+      const escapedWords = words.map(
+        word => word.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+      ).join("|");
+      // Create regex pattern
+      const pattern = `^(${escapedWords})`;
+      // Return the compiled regex
+      return new RegExp(pattern);
+    }
+
+    // this.bot.sendChatAction(chatId, "typing");
+    const checkForKeys = (messageText: string) => {
+      if (messageText.startsWith("/start")) {
+        return "start";
+      }
+      if (messageText.startsWith("/simple")) {
+        return "simple";
+      }
+      if (messageText.startsWith("/turbo")) {
+        return "turbo";
+      }
+      if (messageText.startsWith("/clear")) {
+        return "clear";
+      }
+
+      const regex = createRegexMatchingWords(['/stock', '$stock', '*gpt4', '*gpt'])
+      if (
+        regex.test(messageText)
+      ) {
+        return "skip";
+      }
+      return;
+    };
+
+    const keys = checkForKeys(messageText);
+
+    if (isActivePrivateChat) {
+      console.log('here')
+      switch (keys) {
+        case "start":
+          this.bot.sendMessage(chatId, "Welcome to the bot!");
+          break;
+        case "simple":
+          console.log("simple");
+          const fixed = msg.text.slice(7).trim();
+          const responseTextTurbo = await this.gpt.fetchGptResponseTurbo(
+            fixed,
+            []
+          );
+          this.bot.sendMessage(chatId, responseTextTurbo);
+          break;
+        case "turbo":
+          console.log("turbo");
+          const fixedTurbo = msg.text.slice(6).trim();
+          const interval = setInterval(() => {
+            this.bot.sendChatAction(chatId, "typing");
+          }, 2000);
+          const response = this.gpt.fetchGptResponseTurbo(fixedTurbo, []);
+          clearInterval(interval);
+          this.bot.sendMessage(chatId, response, {
+            parse_mode: "HTML",
+          });
+          break;
+        case "clear":
+          this.db.clearMessages(chatId);
+          this.bot.sendMessage(chatId, "Messages cleared");
+          break;
+        case "skip":
+          // special cases.
+          let data = await this.triggers(messageText, chatId);
+          this.bot.sendMessage(chatId, data, {
+            parse_mode: "HTML",
+          });
+          break;
+        default: {
+          // start typing action
+          const interval = this.startTypingInterval(chatId);
+          // get response
+          const response = await this.handleGPTEventInternal(
+            messageText,
+            chatId,
+            {
+              // model: "gpt-4",
             }
-          }
+          );
+          clearInterval(interval);
+          // send response
+          this.bot.sendMessage(chatId, response, {
+            parse_mode: "HTML",
+          });
+          return;
         }
       }
+    }
 
-      if (response) {
-        this.bot.sendChatAction(chatId, "typing");
-        this.bot.sendMessage(chatId, response, {
-          parse_mode: "HTML",
-        });
-      }
-    });
+    if (isActiveGroupChat && keys === "skip") {
+      // special cases.
+      let data = await this.triggers(messageText, chatId);
+      this.bot.sendMessage(chatId, data, {
+        parse_mode: "HTML",
+      });
+      return;
+    }
+
+  }
+
+  start() {
+    this.bot.on("message", async (arg: any) => {  await this.onMessage(arg) });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ dotenv.config();
 const botToken = process.env.BOT_TOKEN;
 const openAiApiKey = process.env.OPENAI_API_KEY;
 const allowed_members = process.env.ALLOWED_MEMBERS?.split(",");
-
 const botMaster = new BotMaster(
   botToken as string,
   openAiApiKey as string,
